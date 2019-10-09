9dd02d05b4b063d5d16e69b1de3587998ead1855
Esther Wong <ew@Esthers-MacBook-Air.local>
2019-10-09 13:01:59 +1100

    change to vuetify grid

--- src/components/NavGrid.vue
+++ src/components/NavGrid.vue
@@ -1,49 +1,41 @@
 <template>
-    <section class="nav__grid">
-        <a v-for="item in items"  class="nav__box"
-        :href="item.link"
-        :style="'background: url(' + item.img + ');background-size: cover; display: block;'">
-            <div class="nav__description">{{ item.description }}</div>
-        </a>
-    </section>
+    <v-container
+      class="nav__grid"
+      fluid
+    >
+    <v-col>
+      <v-row align="center">
+        <v-card
+          v-for="item in items"
+          :key="item.title"
+          :href="item.link"
+          width="30%"
+          class="ma-3"
+        >
+          <v-img
+            :src="item.src"
+            class="white--text"
+            height="180px"
+            gradient="to bottom, rgba(0,0,0,.3), rgba(0,0,0,.8)"
+          >
+            <v-card-title
+              class="justify-center fill-height align-center"
+              v-text="item.title"
+            ></v-card-title>
+          </v-img>
+        </v-card>
+      </v-row>
+   </v-col>
+   </v-container>
 </template>
 
 
-<style>
+<style scoped>
+
     @import url('https://fonts.googleapis.com/css?family=Quicksand');
 
     .nav__grid{
         font-family: 'Quicksand', sans-serif;
-        display: flex;
-        flex-direction: row;
-        flex-wrap: wrap;
-        padding-bottom: 2%;
-        margin-left: 5%;
-        margin-right: 5%;
-        position: relative;
-    }
-
-    .nav__box{
-        width: calc(80%/3);
-        height: 10em;
-        text-decoration:none;
-        margin: 20px;
-        box-shadow: 0 1px 2px rgba(0,0,0,.1);
-        transition: all 0.3s ease;
-        flex: 0 0 auto;
-    }
-
-    .nav__box:hover{
-        box-shadow: 0 5px 10px #777;
-        transition: all 0.4s ease;
-    }
-
-    .nav__description{
-        padding: 10%;
-        text-align: center;
-        font-weight: bold;
-        color:  #f7f8f9;
-        font-size:  160%;
     }
 
 </style>
@@ -53,8 +45,8 @@ export default {
     name: 'NavGrid',
     data: () => (
         {
-          items: []
-          // items have description, image url, and link
+          items: [],
+          // items have title, image url (src), and link
         }
     ),
 
@@ -62,7 +54,6 @@ export default {
         fetch("")
         .then (r => { return r.json() })
         .then(responseJson => {
-            console.log(responseJson)
             this.items = responseJson
         })
     }
