49c8edb295878fe003585a631d074cc5d61b1049
gawdn <gordon.zhong@outlook.com.au>
2019-10-10 22:11:42 +1100

    increase line len

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'import/extensions': 'off',
-    'max-len': ['error', { code: 120 }],
+    'max-len': ['error', { code: 120, ignoreUrls: true }],
   },
   parserOptions: {
     parser: 'babel-eslint',
