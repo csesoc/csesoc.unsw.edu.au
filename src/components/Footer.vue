16e138b20f6ea2946ff49b9c180eaa9fee0b376d
Esther Wong <ew@Esthers-MacBook-Air.local>
2019-10-24 16:55:28 +1100

    fetching sponsors

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,26 +1,9 @@
-
 <template>
-    <div>
-       <!-- <svg class="defs-only" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-       <symbol id="wave">
-       <svg viewBox="0 0 100 50" preserveAspectRatio="none" >
-          <g>
-        	 <path d="M100,30 Q70,40 50,30 T0,30 v20 h100Z"
-              style="fill:#4a4a4a; stroke-linejoin:round; stroke:#ffffff" stroke-width="0"/>
-          </g>
-       </svg>
-       </symbol>
-       </svg>
-          <svg id="top-wave" width="50" height="50" style="width: 100%; height: 350px">
-          <use xlink:href="#wave"/>
-       </svg> -->
-
     <v-footer
       dark
       padless
       id="tiers"
     >
-
         <v-card
           id="tiers__sponsors"
           width="100%"
@@ -64,25 +47,18 @@
         <v-card-text class="white--text text-center">
           {{ new Date().getFullYear() }} — <strong>CSESoc</strong>
         </v-card-text>
-      </v-card>
+
+        </v-card>
     </v-footer>
-    </div>
 </template>
 
 
 <style scoped>
-
     @import url("https://fonts.googleapis.com/css?family=Quicksand");
 
     #tiers {
         margin-top: 0px;
-        /* border-top-right-radius: 5% 10%;
-        border-top-left-radius: 5% 10%; */
         font-family: "Quicksand", sans-serif;
-    }
-    #top-wave {
-        margin: 0;
-
     }
 
     #tiers__sponsors {
@@ -94,6 +70,7 @@
         font-size: 16px;
         line-height: 28px;
     }
+
     .white--text a {
         color: white;
     }
@@ -110,84 +87,33 @@
         align-items: center;
         margin-left: 1%;
         margin-right: 1%;
-        position: relative;
     }
-
 
     .tiers__box{
         margin: 20px;
     }
-
-
 </style>
 
 <script type="text/javascript">
-
-
 export default {
   name: 'Footer',
   data: () => (
     {
-      sponsors: [
-        {
-          name: 'google', logo: 'https://spinmodern.com/wp-content/uploads/2015/09/google-logo-white.png', tier: 1000, link: 'http://google.com',
-        },
-        {
-          name: 'microsoft', logo: 'https://www.lanmark.com/wp-content/uploads/2018/10/Microsoft-Logo-White.png', tier: 100, link: '',
-        },
-        {
-          name: 'atlassian', logo: 'https://cdn.shortpixel.ai/client/to_webp,q_glossy,ret_img/https://www.generativ.ch/wp-content/uploads/2019/02/Atlassian-vertical-white@2x-rgb.png', tier: 100, link: '',
-        },
-        {
-          name: 'canva', logo: 'https://static.canva.com/static/images/canva_logo_100x100@2x.png', tier: 10, link: '',
-        },
-        {
-          name: 'deloitte', logo: 'https://solsticemedia.com.au/wp-content/uploads/2018/03/Deloitte-logo-white.png', tier: 10, link: '',
-        },
-        {
-          name: 'amazon', logo: 'https://golpc.com/wp-content/uploads/2018/11/amazon-logo-white.png', tier: 10, link: '',
-        },
-        {
-          name: 'jane street', logo: 'https://opensource.janestreet.com/assets/JS_logo-d7838b558a1de6c51553426ab5a2bba474510c41c6a5e910a9e30524a32dec27.png', tier: 1000, link: '',
-        },
-        {
-          name: 'google', logo: 'https://spinmodern.com/wp-content/uploads/2015/09/google-logo-white.png', tier: 1000, link: 'http://google.com',
-        },
-        {
-          name: 'microsoft', logo: 'https://www.lanmark.com/wp-content/uploads/2018/10/Microsoft-Logo-White.png', tier: 100, link: '',
-        },
-        {
-          name: 'atlassian', logo: 'https://cdn.shortpixel.ai/client/to_webp,q_glossy,ret_img/https://www.generativ.ch/wp-content/uploads/2019/02/Atlassian-vertical-white@2x-rgb.png', tier: 100, link: '',
-        },
-        {
-          name: 'canva', logo: 'https://static.canva.com/static/images/canva_logo_100x100@2x.png', tier: 10, link: '',
-        },
-        {
-          name: 'deloitte', logo: 'https://solsticemedia.com.au/wp-content/uploads/2018/03/Deloitte-logo-white.png', tier: 10, link: '',
-        },
-        {
-          name: 'amazon', logo: 'https://golpc.com/wp-content/uploads/2018/11/amazon-logo-white.png', tier: 10, link: '',
-        },
-        {
-          name: 'jane street', logo: 'https://opensource.janestreet.com/assets/JS_logo-d7838b558a1de6c51553426ab5a2bba474510c41c6a5e910a9e30524a32dec27.png', tier: 1000, link: '',
-        },
-        {
-          name: 'commbank', logo: 'https://www.vml.com/australia/sites/vml.australia/files/client/logo/cba-logo.png', tier: 100, link: '',
-        },
-      ],
+        // sponsors have name, logo, 'tier' value, and link
+        sponsors: [],
     }
   ),
   computed: {
+    // functions to determine sizing category of sponsor based on their value
     tierOne() {
-      return this.sponsors.filter(x => x.tier >= 1000);
+        return this.sponsors.filter(x => x.tier >= 1000);
     },
     tierTwo() {
-      return this.sponsors.filter(x => (x.tier >= 100 && x.tier < 1000));
+        return this.sponsors.filter(x => (x.tier >= 100 && x.tier < 1000));
     },
     tierThree() {
-      return this.sponsors.filter(x => (x.tier >= 10 && x.tier < 100));
+        return this.sponsors.filter(x => (x.tier >= 10 && x.tier < 100));
     },
-
   },
   mounted() {
     fetch('')
@@ -198,6 +124,4 @@ export default {
       });
   },
 };
-
-
 </script>
