406aa996bfe3595d21e64c769babf5d1cf9b64d9
Tommy Truong <glebme@yahoo.com>
2020-05-08 15:23:02 +1000

    Add database queries to retrieve sponsor/s

--- server/sponsor.go
+++ server/sponsor.go
@@ -5,21 +5,10 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/google/uuid"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
-
-// GetSponsors - Retrieve a sponsor from the database
-func GetSponsors(collection *mongo.Collection, id string, token string) (Sponsor, error) {
-	parsedID := uuid.Must(uuid.Parse(id))
-
-	var result Sponsor
-	filter := bson.D{{Key: "sponsorid", Value: parsedID}}
-	err := collection.FindOne(context.TODO(), filter).Decode(&result)
-
-	return result, err
-}
 
 // NewSponsors - Add a new sponsor
 func NewSponsors(collection *mongo.Collection, name string, logo string, tier string, expiryStr string, token string) error {
@@ -38,6 +27,31 @@ func NewSponsors(collection *mongo.Collection, name string, logo string, tier st
 	insertResult, err := collection.InsertOne(context.TODO(), sponsor)
 	fmt.Println("Inserted a single document: ", insertResult.InsertedID)
 	return err
+}
+
+// GetSponsor - Retrieve a list of sponsors from the database
+func GetSponsor(collection *mongo.Collection, sponsorName string, token string) (Sponsor, error) {
+	var result Sponsor
+	filter := bson.D{{Key: "sponsorname", Value: sponsorName}}
+	err := collection.FindOne(context.TODO(), filter).Decode(&result)
+
+	return result, err
+}
+
+// GetSponsors - Retrieve a sponsor from the database
+func GetSponsors(collection *mongo.Collection, token string) ([]*Sponsor, error) {
+	var results []*Sponsor
+
+	curr, err := collection.Find(context.TODO(), bson.D{{}}, options.Find())
+	if err == nil {
+		for curr.Next(context.TODO()) {
+
+			var elem Sponsor
+			curr.Decode(&elem)
+			results = append(results, &elem)
+		}
+	}
+	return results, err
 }
 
 // DeleteSponsors - Delete a sponsor from the database
