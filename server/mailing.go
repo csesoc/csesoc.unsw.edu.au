6e8c56aabaf958df6f6647618c901f7ac77df5b9
Sergio Mercado Ruiz <sergio.mercadoruiz9@gmail.com>
2020-05-14 16:15:37 +1000

    Use structs for validation
    The purpose of validating thorugh structs is to simplify and unify the verification process.

--- server/mailing.go
+++ server/mailing.go
@@ -2,26 +2,33 @@ package main
 
 import (
 	"net/http"
-	"regexp"
 
 	"github.com/labstack/echo/v4"
 )
 
+// Message - struct to contain email message data
+type Message struct {
+	Name  string `validate:"required"`
+	Email string `validate:"required,email"`
+	Body  string `validate:"required"`
+}
+
 // HandleEnquiry by forwarding emails to relevant inboxes
 func HandleEnquiry(targetEmail string) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		name := c.FormValue("name")
-		email := c.FormValue("email")
-		message := c.FormValue("message")
-
-		// Email validation
-		emailRegex := regexp.MustCompile(`^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$`)
-
-		if emailRegex.MatchString(email) && len(name) > 0 && len(message) > 0 {
-			// TODO: Compose and send email
-			return c.JSON(http.StatusOK, H{})
-		} else {
-			return c.JSON(http.StatusBadRequest, H{})
+		message := Message{
+			Name:  c.FormValue("name"),
+			Email: c.FormValue("email"),
+			Body:  c.FormValue("body"),
 		}
+
+		if err := c.Validate(message); err != nil {
+			return c.JSON(http.StatusBadRequest, H{
+				"error": err,
+			})
+		}
+
+		// TODO: Compose and send email
+		return c.JSON(http.StatusOK, H{})
 	}
 }
