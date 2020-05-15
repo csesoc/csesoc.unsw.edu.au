d564f967da8fee942c4055cb3f57960b1916a066
Paula Tennent <pjtennent@gmail.com>
2020-05-15 11:58:58 +1000

    created a way to handle feedback form enquiries

--- server/mailing.go
+++ server/mailing.go
@@ -13,6 +13,14 @@ type Message struct {
 	Body  string `validate:"required"`
 }
 
+// Feedback - struct to contain feedback message data
+// name and email are not required
+type Feedback struct {
+	Name  string
+	Email string `validate:"email"`
+	Body  string `validate:"required"`
+}
+
 // HandleEnquiry by forwarding emails to relevant inboxes
 func HandleEnquiry(targetEmail string) echo.HandlerFunc {
 	return func(c echo.Context) error {
@@ -29,6 +37,26 @@ func HandleEnquiry(targetEmail string) echo.HandlerFunc {
 		}
 
 		// TODO: Compose and send email
+		return c.JSON(http.StatusOK, H{})
+	}
+}
+
+// FeedbackEnquiry - validates feedback input and forwards as an email to the revelant inbox
+func FeedbackEnquiry(targetEmail string) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		feedback := Feedback{
+			Name:  c.FormValue("name"),
+			Email: c.FormValue("email"),
+			Body:  c.FormValue("body"),
+		}
+
+		if err := c.Validate(feedback); err != nil {
+			return c.JSON(http.StatusBadRequest, H{
+				"error": err,
+			})
+		}
+
+		//TODO: Compose email and send
 		return c.JSON(http.StatusOK, H{})
 	}
 }
