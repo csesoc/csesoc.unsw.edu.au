ad8e6bc4a9e48b332b75ff9ab72fed5a47545bcb
Paula Tennent <pjtennent@gmail.com>
2020-05-22 20:09:17 +1000

    fixed inputting no email to being valid in mailing.go

--- server/mailing.go
+++ server/mailing.go
@@ -14,10 +14,18 @@ type Message struct {
 }
 
 // Feedback - struct to contain feedback message data
-// name and email are not required
+// name is not required, email must be valid and body is required.
 type Feedback struct {
 	Name  string
 	Email string `validate:"email"`
+	Body  string `validate:"required"`
+}
+
+// FeedbackNoEmail - struct to contain feedback message when there is no email given
+// name, email are not required, body is.
+type FeedbackNoEmail struct {
+	Name  string
+	Email string
 	Body  string `validate:"required"`
 }
 
@@ -44,16 +52,38 @@ func HandleEnquiry(targetEmail string) echo.HandlerFunc {
 // FeedbackEnquiry - validates feedback input and forwards as an email to the revelant inbox
 func FeedbackEnquiry(targetEmail string) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		feedback := Feedback{
-			Name:  c.FormValue("name"),
-			Email: c.FormValue("email"),
-			Body:  c.FormValue("body"),
-		}
+		name := c.FormValue("name")
+		email := c.FormValue("email")
+		body := c.FormValue("body")
 
-		if err := c.Validate(feedback); err != nil {
-			return c.JSON(http.StatusBadRequest, H{
-				"error": err,
-			})
+		// if there is no email, use the struct where emails are not required (FeedbackNoEmail)
+		if email == "" {
+			feedback := FeedbackNoEmail{
+				Name:  name,
+				Email: email,
+				Body:  body,
+			}
+
+			if err := c.Validate(feedback); err != nil {
+				return c.JSON(http.StatusBadRequest, H{
+					"error": err,
+				})
+			}
+
+			// if there is an email, check that the email is valid by using the Feedback Struct
+		} else {
+			feedback := Feedback{
+				Name:  c.FormValue("name"),
+				Email: c.FormValue("email"),
+				Body:  c.FormValue("body"),
+			}
+
+			if err := c.Validate(feedback); err != nil {
+				return c.JSON(http.StatusBadRequest, H{
+					"error": err,
+				})
+			}
+
 		}
 
 		//TODO: Compose email and send
