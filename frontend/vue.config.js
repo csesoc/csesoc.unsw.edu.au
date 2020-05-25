4deaa583fb92eea608868ded2a24f2eb498fb7fd
Tommy Truong <glebme@yahoo.com>
2020-05-25 14:52:32 +1000

    Make changes to dockerfiles to reflect refactor.

---
+++ frontend/vue.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    devServer: {
+        proxy: {
+            '/api': {
+                target: "http://backend:1323",
+                ws: true,
+                changeOrigin: true,
+                headers: {
+                    Connection: 'keep-alive'
+                }
+            }
+        },
+        historyApiFallback: false
+    },
+}
