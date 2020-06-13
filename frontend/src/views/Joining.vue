cd43cf72c51f48705c4022f311dfef50f53786b4
Jarvis Wang <ss96903222@gmail.com>
2020-06-13 14:53:14 +1000

    add some static facebook events cards

--- frontend/src/views/Joining.vue
+++ frontend/src/views/Joining.vue
@@ -1,6 +1,9 @@
 <template>
   <v-app>
-    <v-parallax height="300" src="https://images.unsplash.com/photo-1483577910943-59decea62c35?ixlib=rb-1.2.1&auto=format&fit=crop&w=3889&q=80"></v-parallax>
+    <v-parallax
+      height="300"
+      src="https://images.unsplash.com/photo-1483577910943-59decea62c35?ixlib=rb-1.2.1&auto=format&fit=crop&w=3889&q=80"
+    ></v-parallax>
     <v-expansion-panels flat accordion id="show" style="padding: 20px 20%">
       <v-expansion-panel>
         <v-expansion-panel-header class="title py-3">What is CSESOC?</v-expansion-panel-header>
@@ -16,7 +19,6 @@
           <br />- We're as passionate about computing as you are; we provide technical events to teach you new skills, as well as a fortnightly magazine to keep you updated on what's new and interesting within the university, school and industry.
           <br />
           <br />Most of all, we're here to help you settle in and have a great time at UNSW!
-          
         </v-expansion-panel-content>
       </v-expansion-panel>
       <v-expansion-panel>
@@ -51,12 +53,64 @@
         </v-expansion-panel-content>
       </v-expansion-panel>
     </v-expansion-panels>
+    <v-card class="mx-auto" style="padding: 50px">
+      <v-container fluid>
+        <v-row dense>
+          <v-col v-for="card in cards" :key="card.title" :cols="card.flex">
+            <v-card>
+              <v-img
+                :src="card.src"
+                class="white--text align-end"
+                gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+                height="200px"
+              >
+                <v-card-title v-text="card.title"></v-card-title>
+              </v-img>
+
+              <v-card-actions>
+                <v-spacer></v-spacer>
+
+                <v-btn icon>
+                  <v-icon>mdi-heart</v-icon>
+                </v-btn>
+
+                <v-btn icon>
+                  <v-icon>mdi-bookmark</v-icon>
+                </v-btn>
+
+                <v-btn icon>
+                  <v-icon>mdi-share-variant</v-icon>
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-card>
   </v-app>
 </template>
 
 <script>
 export default {
-  data: () => ({}),
+  data: () => ({
+    cards: [
+        {
+          title: "CSESoc at UNSW O-Week!",
+          src: "https://scontent-syd2-1.xx.fbcdn.net/v/t1.0-9/84027645_2941067712592848_9084286345676849152_o.jpg?_nc_cat=100&_nc_sid=b386c4&_nc_ohc=MKJ-STTDT0QAX_UX1Bp&_nc_ht=scontent-syd2-1.xx&oh=5cec341c7754c26c3e75fe4f09720b69&oe=5F07FB7C ",
+          flex: 4     
+        },
+        {
+          title: "CSESoc Scavenger Hunt!",
+          src: "https://scontent-syd2-1.xx.fbcdn.net/v/t1.0-9/s960x960/100995672_3310483902317892_7839912922775027712_o.jpg?_nc_cat=108&_nc_sid=b386c4&_nc_ohc=aYkRm1P5zvkAX_NNU6A&_nc_ht=scontent-syd2-1.xx&_nc_tp=7&oh=d4aaaf8c745f38acf1e8533e5a89ace9&oe=5F0A0545",
+          flex: 4
+        },
+        {
+          title: "CSESoc Hoodie Preorders!",
+          src: "https://scontent-syd2-1.xx.fbcdn.net/v/t1.0-9/97431643_3260560320643584_6121191148355584000_o.jpg?_nc_cat=103&_nc_sid=b386c4&_nc_ohc=Keipx6N9K8EAX_zlnPw&_nc_ht=scontent-syd2-1.xx&oh=0836a183f2f0774de5e022ebc3e2549f&oe=5F0A9711 ",
+          flex: 4
+        }
+      ]
+    }),
   components: []
 };
 </script>
