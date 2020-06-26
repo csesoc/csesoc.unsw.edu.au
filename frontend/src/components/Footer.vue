9746be72cdc92677bde355e92e730e73a1913dcc
boboeee <jiechen4th@gmail.com>
2020-06-26 13:25:52 +1000

    Finished initial sponsors page

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -3,18 +3,18 @@
     <v-card id="tiers__sponsors" width="100vw" class="white--text text-center">
       <v-card-text class="white--text">
         <section class="tiers__grid">
-          <a v-for="sponsor in tierOne" class="tiers__box" :href="sponsor.link" :key="sponsor.id">
-            <img :src="`data:image/png;base64,${sponsor.logo}`" style=" max-width:300px;max-height:105px;" />
+          <a v-for="sponsor in tierOne" class="tiers__box" :href="sponsor.url" :key="sponsor.id">
+            <img :src="sponsor.logo" style=" max-width:300px;max-height:105px;" />
           </a>
         </section>
         <section class="tiers__grid">
-          <a v-for="sponsor in tierTwo" class="tiers__box" :href="sponsor.link" :key="sponsor.id">
-            <img :src="`data:image/png;base64,${sponsor.logo}`" style="max-width:200px;max-height:75px" />
+          <a v-for="sponsor in tierTwo" class="tiers__box" :href="sponsor.url" :key="sponsor.id">
+            <img :src="sponsor.logo" style="max-width:200px;max-height:75px" />
           </a>
         </section>
         <section class="tiers__grid">
-          <a v-for="sponsor in tierThree" class="tiers__box" :href="sponsor.link" :key="sponsor.id">
-            <img :src="`data:image/png;base64,${sponsor.logo}`" style="max-width:100px;max-height:50px" />
+          <a v-for="sponsor in tierThree" class="tiers__box" :href="sponsor.url" :key="sponsor.id">
+            <img :src="sponsor.logo" style="max-width:100px;max-height:50px" />
           </a>
         </section>
       </v-card-text>
