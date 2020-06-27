7521943c88a50ad4fe18475fcf64c9171a0758c1
Sergio Mercado Ruiz <sergio.mercadoruiz9@gmail.com>
2020-06-27 12:02:32 +1000

    CW-111 Update JWT's expiry time
    JWTs expired yesterday. I added an extra 2 months to their expiry time.

--- backend/server/constants.go
+++ backend/server/constants.go
@@ -6,6 +6,6 @@ const SPONSOR_URL = "api/v1/sponsors"
 const MAILING_URL = "api/v1/mailing"
 const SOCIAL_URL = "api/v1/social"
 const FAQ_URL = "api/v1/faq"
-const AUTH_TOKEN = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhZG1pbiI6dHJ1ZSwiZXhwIjoxNTkzMTI5NjAwLCJ6SUQiOiJ6NTEyMzQ1NiJ9.jYC2qlpzAKIMPFywQ6pWIV1qat_h7OrorJ-zQM5jDpg"
+const AUTH_TOKEN = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhZG1pbiI6dHJ1ZSwiZXhwIjoxNTk4Mzg5MDg2LCJ6SUQiOiJ6NTEyMzQ1NiJ9.a8869YcfgBZCQcWUFuomF4Fqx9nJzT-rWpP2yRVsBWg"
 
 var JWT_SECRET = []byte("temp_secret_until_proper_secrets_are_implemented")
