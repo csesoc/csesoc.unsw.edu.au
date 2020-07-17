84be6c5f27411a18acba6d6eadb4fe42ebb1fafc
Tommy Truong <glebme@yahoo.com>
2020-07-17 23:05:09 +1000

    Add Vue testing - jest.

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,12 +1,15 @@
 module.exports = {
   root: true,
+
   env: {
     node: true,
   },
+
   extends: [
     'plugin:vue/essential',
     '@vue/airbnb',
   ],
+
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
@@ -18,7 +21,20 @@ module.exports = {
     ],
     'comma-dangle': 'off'
   },
+
   parserOptions: {
     parser: 'babel-eslint',
   },
+
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
+      ],
+      env: {
+        jest: true
+      }
+    }
+  ]
 };
