f485457ded734fbdc53db8bea02cde637e770b98
Tommy Truong <glebme@yahoo.com>
2020-07-20 12:45:57 +1000

    Implement preview image container.

--- frontend/src/components/Preview.vue
+++ frontend/src/components/Preview.vue
@@ -1,22 +1,36 @@
 <template>
   <v-container>
-    <v-list two-line>
-      <template v-for="(item, index) in items">
-        <v-list-item :key="item.title">
-          <v-list-item-content>
-            <v-list-item-title v-html="item.title"></v-list-item-title>
-            <v-list-item-subtitle v-html="item.description"></v-list-item-subtitle>
-          </v-list-item-content>
-          <v-divider :key="index" />
-        </v-list-item>
-      </template>
-    </v-list>
+    <v-row align="center" justify="center">
+      <v-col>
+        <v-row justify="center">
+          <v-container class="preview-container">
+            <router-link to="/">
+              <v-img :src="`data:image/png;base64,${preview}`"/>
+            </router-link>
+          </v-container>
+        </v-row>
+      </v-col>
+      <v-col>
+        <v-list two-line>
+          <template v-for="(item, index) in items">
+            <v-list-item @mouseover="preview = item.image" :key="item.title">
+              <v-list-item-content>
+                <v-list-item-title v-html="item.title"></v-list-item-title>
+                <v-list-item-subtitle v-html="item.description"></v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+            <v-divider :key="index" />
+          </template>
+        </v-list>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script>
 export default {
   data: () => ({
+    preview: "@/assets/csesocgreyblue.png",
     items: [
       {
         title: 'First Year Guide.',
@@ -39,6 +53,8 @@ export default {
 };
 </script>
 
-<style lang='stylus' scoped>
-
+<style scoped>
+  .preview-container {
+    max-width: 600px;
+  }
 </style>
