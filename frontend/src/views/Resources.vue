67fa784db924f662d49bb1b8d5fb5c580131e40a
Tommy Truong <glebme@yahoo.com>
2020-08-15 02:46:31 +1000

    Fix text scaling and add space values.

--- frontend/src/views/Resources.vue
+++ frontend/src/views/Resources.vue
@@ -1,6 +1,10 @@
 <template>
   <v-container>
-    <h1>Hi</h1>
+    <h1>Heading 1</h1>
+    <h2>Heading 2</h2>
+    <h3>Heading 3</h3>
+    <h4>Heading 4</h4>
+    <p>Paragraph</p>
     <!-- <v-row>
       <v-col class="pa-4" cols='6'>
         Some text
