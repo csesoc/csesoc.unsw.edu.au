0e8be636eff0fbbcce66b4e3b50a55708ec3ba93
Paula Tennent <pjtennent@gmail.com>
2020-08-19 20:23:18 +1000

    community links are now a component

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -85,29 +85,8 @@
       <Preview :items="resourceItems"/>
     </v-container>
 
-    <!-- Join Us Section -->
-    <v-container id="join" ref="joinus">
-      <h1>
-        Join Us
-      </h1>
-      <p>
-        Find our stall at O-week, or just turn up an event and get to know us!
-      </p>
-      <h2>
-        Join our community online
-      </h2>
-      <div id="joinImg" >
-        <a href="https://www.facebook.com/groups/csesoc">
-          <img src="@/assets/facebook_logo.png" class="zoom">
-        </a>
-        <a href="https://csesoc-community.slack.com/">
-          <img src="@/assets/slack_logo.png" class="zoom">
-        </a>
-        <!-- need to add in the csesoc discord link-->
-        <a href="https://www.facebook.com/groups/csesoc">
-          <img src="@/assets/discord_logo.png" class="zoom">
-        </a>
-      </div>
+    <v-container>
+      <CommunityLinks></CommunityLinks>
     </v-container>
 
     <!-- Support CSESoc -->
@@ -139,6 +118,7 @@ import EventGrid from '@/components/EventGrid';
 import InfiniteSlideshow from '@/components/InfiniteSlideshow';
 import Preview from '@/components/Preview';
 import APIClient from '../utils/APIClient';
+import CommunityLinks from '@/components/CommunityLink';
 
 export default {
   data: () => ({
@@ -158,7 +138,8 @@ export default {
     HeaderTitle,
     EventGrid,
     InfiniteSlideshow,
-    Preview
+    Preview,
+    CommunityLinks
   },
   mounted() {
     fetch(this.mediaApiUri)
@@ -254,44 +235,6 @@ export default {
   align-items: center;
   text-align: center;
   padding: 5vh 11vw;
-}
-
-#join {
-  align-items: center;
-  text-align: center;
-}
-
-#join h1 {
-  font-size: 80px;
-}
-
-#join h2 {
-  font-size: 40px;
-  padding-bottom: 5vh;
-}
-
-#join p {
-  font-size: 35px;
-  padding: 0 15vw 3vh;
-}
-
-#join img {
-  width: 20%;
-  height: auto;
-  padding-left: 4%;
-  padding-right: 4%;
-}
-
-#joinImg {
-  padding-bottom: 5%;
-}
-
-.zoom {
-  transition: transform .2s;
-}
-
-.zoom:hover {
-  transform: scale(1.1);
 }
 
 #sponsor {
