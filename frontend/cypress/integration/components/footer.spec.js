a667945628309c58678f8d9f7a511c384eb59d12
Jarvis Wang <ss96903222@gmail.com>
2020-08-20 23:23:14 +1000

    Add testing for internal links

--- frontend/cypress/integration/components/footer.spec.js
+++ frontend/cypress/integration/components/footer.spec.js
@@ -12,9 +12,16 @@ describe("Footer Testing", () => {
 
         // Check if the internal link is displayed
 
-        // cy.get('[data-cy=footer-internal-link]').should( ($lis) => {
-            // expect($lis, '4 items').to.have.length(4);
-            // expect($lis, '')
-        // })
+        cy.get('[data-cy=footer-internal-link]').should( ($lis) => {
+            expect($lis, '4 items').to.have.length(4);
+            expect($lis.eq(0), 'first item').to.have.attr('href', 'localhost:8080/about');
+            expect($lis.eq(0), 'first item').to.have.text('About');
+            expect($lis.eq(1), 'second item').to.have.attr('href', 'localhost:8080/resources');
+            expect($lis.eq(1), 'second item').to.have.text('Resources');
+            expect($lis.eq(2), 'third item').to.have.attr('href', 'localhost:8080/sponsor');
+            expect($lis.eq(2), 'third item').to.have.text('Sponsor');
+            expect($lis.eq(3), 'forth item').to.have.attr('href', 'localhost:8080/engage');
+            expect($lis.eq(3), 'first item').to.have.text('Engage');
+        });
     });
 })
