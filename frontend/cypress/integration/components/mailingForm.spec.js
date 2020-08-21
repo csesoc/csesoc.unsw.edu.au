8a4cb6880c2c783a46ad110db3841c7dc1f8a227
Sergio Mercado Ruiz <sergio.mercadoruiz9@gmail.com>
2020-08-21 21:38:26 +1000

    CW-177 Simplify testing from feedback given to improve performance and reduce repetition

--- frontend/cypress/integration/components/mailingForm.spec.js
+++ frontend/cypress/integration/components/mailingForm.spec.js
@@ -1,5 +1,5 @@
 describe('Mailing forms', () => {
-  it('checks labels and validation of a general form', () => {
+  it('checks labels and validation of a form', () => {
     // Visit engage page
     cy.visit('/#/engage');
     // By referencing general-tab items we are ensuring that it is selected by default
@@ -13,38 +13,24 @@ describe('Mailing forms', () => {
     cy.get('[data-cy=general-send-button]').should('be.disabled');
     // Fill in valid name
     cy.get('[data-cy=general-name-field]').type('Sergio');
-    // Fill in valid email
-    cy.get('[data-cy=general-email-field]').type('sergio@gmail.com');
+    // Fill in invalid email
+    cy.get('[data-cy=general-email-field]').type('sergio2@ema.il.c');
     // Fill in valid message
     cy.get('[data-cy=general-message-field]').type('message goes here');
-    // Ensure send button is enabled after entering valid inputs
+    // Ensure send button is disabled,
+    // because the email is invalid
+    cy.get('[data-cy=general-send-button]').should('be.disabled');
+    // Fill in valid email
+    cy.get('[data-cy=general-email-field]').clear().type('sergio@gmail.com');
+    // Ensure send button is enabled after making the email valid
     cy.get('[data-cy=general-send-button]').should('not.be.disabled');
   });
 
-  it('checks labels and validation of a sponsorship form', () => {
+  it('ensures the name label is different in a sponsorship form', () => {
     // Visit sponsors page
     cy.visit('/#/sponsors');
-    // Check if name label exists
+    // Check if name label changed to the sponsorship form version
     cy.get('[data-cy=sponsorship-name-label]').contains('Company Name *');
-    // Check if email label exists
-    cy.get('[data-cy=sponsorship-email-label]').contains('Email *');
-    // Check if message label exists
-    cy.get('[data-cy=sponsorship-message-label]').contains('Message *');
-    // Ensure send button is disabled
-    cy.get('[data-cy=sponsorship-send-button]').should('be.disabled');
-    // Fill in valid name
-    cy.get('[data-cy=sponsorship-name-field]').type('Sergio');
-    // Fill in invalid email
-    cy.get('[data-cy=sponsorship-email-field]').type('sergio2@ema.il.c');
-    // Fill in valid message
-    cy.get('[data-cy=sponsorship-message-field]').type('message goes here');
-    // Ensure send button is disabled,
-    // because the email is invalid
-    cy.get('[data-cy=sponsorship-send-button]').should('be.disabled');
-    // Fill in valid email
-    cy.get('[data-cy=sponsorship-email-field]').clear().type('sergio@gmail.com');
-    // Ensure send button is enabled after making the email valid
-    cy.get('[data-cy=sponsorship-send-button]').should('not.be.disabled');
   });
 
   it('checks labels and validation of a feedback form', () => {
@@ -52,18 +38,16 @@ describe('Mailing forms', () => {
     cy.visit('/#/engage');
     // Select feedback form tab
     cy.get('[data-cy=feedback-form-tab]').click();
-    // Check if name label exists
+    // Check if name label is not required (marked with *)
     cy.get('[data-cy=feedback-name-label]').contains('Name');
-    // Check if email label exists
+    /// Check if email label is not required (marked with *)
     cy.get('[data-cy=feedback-email-label]').contains('Email');
-    // Check if message label exists
-    cy.get('[data-cy=feedback-message-label]').contains('Message *');
     // Ensure send button is disabled
     cy.get('[data-cy=feedback-send-button]').should('be.disabled');
     // Fill in valid message
     cy.get('[data-cy=feedback-message-field]').type('message goes here');
     // Ensure send button is enabled,
-    // because the message is the only required field
+    // because message is the only required field
     cy.get('[data-cy=feedback-send-button]').should('not.be.disabled');
   });
 });
