a9e527111390d2f6374f9ffa70477acecc0e8678
Paula Tennent <pjtennent@gmail.com>
2020-08-29 03:27:44 +1000

    added comments to error in community links testing

--- frontend/cypress/integration/components/communityLinks.spec.js
+++ frontend/cypress/integration/components/communityLinks.spec.js
@@ -29,6 +29,10 @@ describe('Community Links Landing Page', () => {
 
     it('tests scrolling to the join us section from the top of the landing page', () => {
       // finding the join us button and clicking it
+
+      // cypress is giving a 'the chainer inViewPort cannot be found', 
+      // might try making it a command not an assert to see if that fixes it
+      // ref: https://github.com/cypress-io/cypress/issues/877
       cy.get('[data-cy=communitLinks]').should('not.be.inViewPort');
       cy.get('[data-cy=joinus-button]').click();
       // it should scroll to the join us section
