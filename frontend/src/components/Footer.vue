da48c15c70c32deedbc33c95bb136d554caffb8c
boboeee <jiechen4th@gmail.com>
2020-08-31 11:23:54 +1000

    inital framework for footer

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -5,32 +5,58 @@
 -->
 
 <template>
-  <v-footer dark padless id="tiers">
-    <v-card id="tiers__sponsors" width="100vw" class="white--text text-center">
-      <v-card-text class="white--text">
-        <v-container class="fill-height" fluid style="max-height: 200px; max-width:300px">
+  <v-footer dark padless id="tiers" >
+    <v-card style="padding:75px;width:100%">
+
+      <v-card-text class="white--text" style="float:left;width:25%">
+        <v-container class="fill-height" fluid style="float:left;max-height: 200px;max-width:300px;">
           <v-img src="@/assets/csesocwhiteblue.png" />
-          <br />B03 CSE Building K17, UNSW
-          <br />
+          <br/>B03 CSE Building K17, 
+          <br/> UNSW
+          <br/>
         </v-container>
       </v-card-text>
-
-      <h2>CONTACT US</h2>
-      <v-card-text class="white-text text-center tiers-contact">
-        <a href="https://www.facebook.com/csesoc">Facebook Page</a>
-        |
-        <a href="https://www.facebook.com/groups/csesoc">Facebook Group</a>
-        |
-        <a href="https://www.linkedin.com/company/csesoc/about/">LinkedIn</a>
-
-        <a id="tiers__link" href="mailto:csesoc@csesoc.org.au">csesoc@csesoc.org.au</a>
+      <v-card-text class="white--text" style="float:left;width:20%">
+        <a href="https://www.facebook.com/csesoc">About</a>
+        <br/>
+        <a href="https://www.facebook.com/groups/csesoc">Events</a>
+        <br/>
+        <a href="https://www.linkedin.com/company/csesoc/about/">CSE Resources</a>
+        <br/>
+        <a id="tiers__link" href="mailto:csesoc@csesoc.org.au">Sponsorship</a>
+        <br/>
+        <a id="tiers__link" href="mailto:csesoc@csesoc.org.au">Engage</a>
       </v-card-text>
+      <v-card-text class="white--text" style="float:left;width:20%">
+        <a href="https://www.facebook.com/csesoc">Facebook Page</a>
+        <br/>
+        <a href="https://www.facebook.com/groups/csesoc">Instagram</a>
+        <br/>
+        <a href="https://www.linkedin.com/company/csesoc/about/">LinkedIn</a>
+        <br/>
+        <a id="tiers__link" href="mailto:csesoc@csesoc.org.au">Twitter</a>
+        <br/>
+        <a id="tiers__link" href="mailto:csesoc@csesoc.org.au">Youtube</a>
+      </v-card-text>
+<<<<<<< HEAD
 
       <v-divider></v-divider>
 
       <v-card-text class="white-text text-center">
         &copy; {{ new Date().getFullYear() }} &mdash;
         <strong>CSESoc UNSW</strong>
+=======
+      <v-card-text class="white--text" style="float:left;width:25%">
+        <a href="https://www.facebook.com/csesoc">Notangle</a>
+        <br/>
+        <a href="https://www.facebook.com/groups/csesoc">Compclub</a>
+        <br/>
+        <a href="https://www.linkedin.com/company/csesoc/about/">Job Board</a>
+        <br/>
+        <a id="tiers__link" href="mailto:csesoc@csesoc.org.au">CSE UNSW</a>
+        <br/>
+        <a id="tiers__link" href="mailto:csesoc@csesoc.org.au">CSESoc Media</a>
+>>>>>>> inital framework for footer
       </v-card-text>
     </v-card>
   </v-footer>
@@ -67,11 +93,15 @@ export default {
 </script>
 
 <style scoped>
+<<<<<<< HEAD
 #tiers {
   margin-top: 0px;
 }
 
 #tiers-sponsors {
+=======
+#tiers__sponsors {
+>>>>>>> inital framework for footer
   background: #4a4a4a;
   padding-top: 5vw;
 }
@@ -102,8 +132,11 @@ export default {
 .tiers-box {
   margin: 20px;
 }
+<<<<<<< HEAD
 
 .tiers-contact {
   margin-bottom: 1vw;
 }
+=======
+>>>>>>> inital framework for footer
 </style>
