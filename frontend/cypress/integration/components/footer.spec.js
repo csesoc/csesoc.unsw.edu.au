7f6338e0c5db29ea69d3a9cf77b2c5371dee89be
Jarvis Wang <ss96903222@gmail.com>
2020-09-02 20:32:03 +1000

    Add comment for the internal link test

--- frontend/cypress/integration/components/footer.spec.js
+++ frontend/cypress/integration/components/footer.spec.js
@@ -13,21 +13,38 @@ describe("Footer Testing", () => {
         // Check if the internal link is displayed
 
         cy.get('[data-cy=footer-internal-link]').should( ($lis) => {
-            expect($lis, '4 items').to.have.length(4);
-            expect($lis.eq(0), 'first item').to.have.attr('href', 'localhost:8080/about');
-            expect($lis.eq(0), 'first item').to.have.text('About');
-            expect($lis.eq(1), 'second item').to.have.attr('href', 'localhost:8080/resources');
-            expect($lis.eq(1), 'second item').to.have.text('Resources');
-            expect($lis.eq(2), 'third item').to.have.attr('href', 'localhost:8080/sponsor');
-            expect($lis.eq(2), 'third item').to.have.text('Sponsor');
-            expect($lis.eq(3), 'forth item').to.have.attr('href', 'localhost:8080/engage');
-            expect($lis.eq(3), 'first item').to.have.text('Engage');
+            expect($lis, '5 items').to.have.length(5);
+            // Check if the link is correctly linked
+            expect($lis.eq(0), 'first item').to.have.attr('href', 'localhost:8080/');
+            // Check if the text is correctly displayed
+            expect($lis.eq(0), 'first item').to.have.text('Home');
+            // Check if the link is correctly linked
+            expect($lis.eq(1), 'second item').to.have.attr('href', 'localhost:8080/about');
+            // Check if the text is correctly displayed
+            expect($lis.eq(1), 'second item').to.have.text('About');
+            // Check if the link is correctly linked
+            expect($lis.eq(2), 'third item').to.have.attr('href', 'localhost:8080/resources');
+            // Check if the text is correctly displayed
+            expect($lis.eq(2), 'third item').to.have.text('Resources');
+            // Check if the link is correctly linked
+            expect($lis.eq(3), 'forth item').to.have.attr('href', 'localhost:8080/sponsor');
+            // Check if the text is correctly displayed
+            expect($lis.eq(3), 'forth item').to.have.text('Sponsor');
+            // Check if the link is correctly linked
+            expect($lis.eq(4), 'fifth item').to.have.attr('href', 'localhost:8080/engage');
+            // Check if the text is correctly displayed
+            expect($lis.eq(4), 'fifth item').to.have.text('Engage');
         });
 
         // Check if the external link is displayed and the logo is displayed
 
-        cy.get('[data-cy=footer-external-link]').should(($lis) => {
-            expect($lis, '10 items').to.have.length(10);
+        cy.get('[data-cy=footer-social-media-link]').should(($lis) => {
+            expect($lis, '8 items').to.have.length(8);
+            expect($lis.eq(0), 'first item').to.have.attr('href', 'https://www.facebook.com/csesoc');
+            expect($lis.eq(0), 'first item').to.have.text('Facebook Page');
+            expect($lis.eq(1), 'second item').to.have.attr('href', 'https://www.facebook.com/csesoc');
+            expect($lis.eq(1), 'second item').to.have.text('Facebook Page');
+
         });
     });
 })
