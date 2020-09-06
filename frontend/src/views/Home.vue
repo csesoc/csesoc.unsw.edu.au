b3e74d82c21af89596e28c3c35fb0e97ddca076f
Paula Tennent <pjtennent@gmail.com>
2020-09-06 23:08:17 +1000

    add icon images

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -95,11 +95,21 @@
     <!-- Sponsor -->
     <div class=sponsor >
       <h1 class=sponsor--h1>SUPPORT CSESOC</h1>
-      <div class=sponsor-block1>
-        <h4 class=sponsor--h4>Reach Australia's Best Computing Graduates</h4>
+      <div class=sponsor--block1>
+        <img src="@/assets/share.svg" class="sponsor--img">
+        <div class=sponsor--text>
+          <h4 class=sponsor--h4>Reach Australia's Best Computing Graduates</h4>
+          <p class=sponsor--p> Many of our past members have gone on to work at our previous
+            sponsor companies including Google, Facebook, Jane St and Commonwealth Bank. </p>
+        </div>
       </div>
-      <div class=sponsor-block2>
-        <h4 class=sponsor--h4>Interact With Our Huge Active Community</h4>
+      <div class=sponsor--block2>
+        <img src="@/assets/trophy.svg" class="sponsor--img">
+        <div class=sponsor--text>
+          <h4 class=sponsor--h4>Interact With Our Huge Active Community</h4>
+          <p class=sponsor--p> We have an extremely active community of ~3000 CSE students,
+            achieved through our offering of career, social and educational events. </p>
+        </div>
       </div>
       <RouterLink to="/sponsors" class="link" >
         <button class="btn--sponsorUs">
@@ -107,7 +117,6 @@
         </button>
       </RouterLink>
     </div>
-
   </div>
 </template>
 
@@ -282,18 +291,36 @@ export default {
 }
 
 .sponsor--h4 {
-  padding-left: 33%;
+  //padding-left: 33%;
   @extend h4;
   color: $light-color;
-  margin-bottom: $space-md;
+  margin-bottom: $space-xxs;
 }
 
-.sponsor-block1 {
-  margin-bottom: $space-xs;
+.sponsor--p {
+  @extend p;
+  color: $light-color;
 }
 
-.sponsor-block1 {
-  margin-bottom: $space-md;
+.sponsor--text {
+  //width: 70%;
+  opacity: 1;
+  float: left;
+  width: 70%;
+}
+
+.sponsor--img {
+  height: 100px;
+  float: left;
+  width: 30%;
+}
+
+.sponsor--block1 {
+  padding-bottom: $space-md;
+}
+
+.sponsor--block2 {
+  padding-bottom: $space-md;
 }
 
 .btn--sponsorUs {
