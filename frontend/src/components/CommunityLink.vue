ae442349adb5d6d160bf6744dc476462d36fda28
Paula Tennent <pjtennent@gmail.com>
2020-09-07 14:32:06 +1000

    add text below join us icons

--- frontend/src/components/CommunityLink.vue
+++ frontend/src/components/CommunityLink.vue
@@ -4,19 +4,22 @@
     <v-container data-cy="communityLinks" id="join">
       <v-row class="pa-0">
         <v-col cols="4" align="center" class="pa-0">
-          <a href="https://www.facebook.com/groups/csesoc">
-            <img src="@/assets/facebook_logo.png" class="zoom" data-cy="communityLinks-facebookImage">
-          </a>
-        </v-col>
-        <v-col cols="4" align="center" class="pa-0">
           <a href="https://csesoc-community.slack.com/">
             <img src="@/assets/slack_logo.png" class="zoom" data-cy="communityLinks-slackImage">
           </a>
+          <h2>Slack Community</h2>
+        </v-col>
+        <v-col cols="4" align="center" class="pa-0">
+          <a href="https://www.facebook.com/groups/csesoc">
+            <img src="@/assets/facebook_logo.png" class="zoom" data-cy="communityLinks-facebookImage">
+          </a>
+          <h2>Facebook Group</h2>
         </v-col>
         <v-col cols="4" align="center" class="pa-0">
           <a href="https://www.facebook.com/groups/csesoc">
             <img src="@/assets/discord_logo.png" class="zoom" data-cy="communityLinks-discordImage">
           </a>
+          <h2>Discord Server</h2>
         </v-col>
       </v-row>
     </v-container>
@@ -30,8 +33,13 @@
 #join img {
   width: 150px;
   height: auto;
+  margin-bottom: $space-xxs
   //padding-left: 4%;
   //padding-right: 4%;
+}
+
+h2 {
+  color: $light-color;
 }
 
 .zoom {
