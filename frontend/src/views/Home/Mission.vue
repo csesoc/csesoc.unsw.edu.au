4b77e6e4db6f1baaf9ad4481a7f9cccd3c863d81
Tommy Truong <glebme@yahoo.com>
2020-10-25 14:06:20 +1100

    Add mission image to landing page.

--- frontend/src/views/Home/Mission.vue
+++ frontend/src/views/Home/Mission.vue
@@ -9,9 +9,12 @@
     <v-container>
     <HeaderTitle :title="'CSESOC\'S MISSION'" />
     <v-row no-gutters justify="end">
-      <v-col cols="12" class="mission--p">
+      <v-col xs="12" sm="6">
+        <v-img contain :src="mission.image" class="image"/>
+      </v-col>
+      <v-col xs="12" sm="6" align-self="center">
         <p class="text--md">
-          {{mission}}
+          {{mission.statement}}
         </p>
       </v-col>
     </v-row>
@@ -28,15 +31,23 @@ export default {
     HeaderTitle
   },
   data: () => ({
-    mission: '"To empower every person and every organization on the planet to achieve more. We strive to create local opportunity, growth, and impact in every country around the world."',
+    mission: {
+      image: require('../../assets/landing_page/mission.png'),
+      statement: '"To empower every person and every organization on the planet to achieve more. We strive to create local opportunity, growth, and impact in every country around the world."',
+    },
   }),
 };
 </script>
 <style lang="scss" scoped>
 #mission {
-//   background-image: linear-gradient($dark-color-1, transparent, $dark-color-1), url(../../assets/mission.jpg);
-  height: 100vh;
-//   background-size: 50%;
-//   padding-top: $space-sm;
+  // background-image: linear-gradient($dark-color-1, transparent, $dark-color-1), url(../../assets/mission.jpg);
+  // height: 100vh;
+  // background-size: 50%;
+  // padding-top: $space-sm;
+}
+
+.image {
+  max-width: 100%;
+  max-height: 100%;
 }
 </style>
