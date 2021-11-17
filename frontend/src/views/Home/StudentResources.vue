f140c0c3cdf1805cb97e3692fff4632dc7f0e83e
Jasper Di Francesco <jasper.difrancesco@mail.com>
2021-11-18 01:41:04 +1100

    Adjust box sizing

--- frontend/src/views/Home/StudentResources.vue
+++ frontend/src/views/Home/StudentResources.vue
@@ -10,58 +10,60 @@
     <v-container class="resource--styles">
       <HeaderTitle title="STUDENT RESOURCES" />
 
-      <v-row justify="space-between">
-        <v-col xs="12" sm="4"> <!-- Jobs -->
+      <v-row  justify="space-between">
+        <v-col cols="12" sm="4"> <!-- Jobs -->
           <a target="_blank" :href="resources.jobs_board.link">
-          <div data-cy="resources-box" class="box big rounded-lg">
+          <div data-cy="resources-box" class="box primary rounded-lg">
             <h2>{{resources.jobs_board.title}}</h2>
             <h3>{{resources.jobs_board.description}}</h3>
-            <v-img data-cy="resources-img" :src="resources.jobs_board.src" class="image hidden-xs-only" />
+            <v-img data-cy="resources-img" max-height="80px" :src="resources.jobs_board.src" class="image hidden-xs-only" />
           </div>
           </a>
         </v-col>
         <v-col cols="12" sm="4"> <!-- CompClub -->
           <a target="_blank" :href="resources.comp_club.link">
-          <div data-cy="resources-box" class="box small rounded-lg">
+          <div data-cy="resources-box" class="box secondary rounded-lg">
             <h2>{{resources.comp_club.title}}</h2>
             <h3 class="hidden-sm-above">{{resources.comp_club.description}}</h3>
-            <v-img data-cy="resources-img" :src="resources.comp_club.src"  class="image hidden-xs-only" />
+            <div class="flex-grow-1"></div>
+            <v-img data-cy="resources-img" max-height="80px" :src="resources.comp_club.src"  class="image hidden-xs-only" />
           </div>
           </a>
         </v-col>
         <v-col cols="12" sm="4"> <!-- Notangles -->
           <a target="_blank" :href="resources.notangles.link">
-          <div data-cy="resources-box" class="box small rounded-lg">
+          <div data-cy="resources-box" class="box primary rounded-lg">
             <h2>{{resources.notangles.title}}</h2>
             <h3>{{resources.notangles.description}}</h3>
-            <v-img data-cy="resources-img" :src="resources.notangles.src" class="image hidden-xs-only" />
+            <v-img data-cy="resources-img" max-height="80px" :src="resources.notangles.src" class="image hidden-xs-only" />
           </div>
           </a>
         </v-col>
-        <v-col cols="12"> <!-- Media -->
+        <v-col cols="12" sm="12"> <!-- Media -->
           <a target="_blank" :href="resources.media.link">
-          <div data-cy="resources-box" class="box small rounded-lg">
+          <div data-cy="resources-box" class="box secondary rounded-lg">
             <h2>{{resources.media.title}}</h2>
             <h3>{{resources.media.description}}</h3>
             <v-img data-cy="resources-img" max-height="100px" :src="resources.media.src" class="image hidden-xs-only" />
           </div>
           </a>
         </v-col>
-        <v-col xs="12" sm="6"> <!-- First year guide -->
-          <a target="_blank" :href="resources.first_year_guide.link">
-          <div data-cy="resources-box" class="box big rounded-lg">
-            <h2>{{resources.first_year_guide.title}}</h2>
-            <h3>{{resources.first_year_guide.description}}</h3>
-            <v-img data-cy="resources-img" :src="resources.first_year_guide.src" class="image hidden-xs-only" />
+        <v-col cols="12" sm="6"> <!-- First year guide -->
+          <a target="_blank" :href="resources.fy_guide.link">
+          <div data-cy="resources-box" class="box primary rounded-lg">
+            <h2>{{resources.fy_guide.title}}</h2>
+            <h3>{{resources.fy_guide.description}}</h3>
+            <v-img data-cy="resources-img" max-height="80px" :src="resources.fy_guide.src" class="image hidden-xs-only"/>
           </div>
           </a>
         </v-col>
-        <v-col xs="12" sm="6"> <!-- Preenrollment guide -->
-          <a target="_blank" :href="resources.pre_enrollment_guide.link">
-          <div data-cy="resources-box" class="box big rounded-lg">
-            <h2>{{resources.pre_enrollment_guide.title}}</h2>
-            <h3>{{resources.pre_enrollment_guide.description}}</h3>
-            <v-img data-cy="resources-img" :src="resources.pre_enrollment_guide.src" class="image hidden-xs-only" />
+        <v-col cols="12" sm="6"> <!-- Preenrollment guide -->
+          <a target="_blank" :href="resources.pe_guide.link">
+          <div data-cy="resources-box" class="box secondary rounded-lg">
+            <h2>{{resources.pe_guide.title}}</h2>
+            <h3>{{resources.pe_guide.description}}</h3>
+            <div class="flex-grow-1"></div>
+            <v-img data-cy="resources-img" max-height="80px" :src="resources.pe_guide.src" class="image hidden-xs-only" />
           </div>
           </a>
         </v-col>
@@ -85,7 +87,7 @@ export default {
         description: 'A place where CSESoc students can look for relevant job opportunities.',
         src: require('../../assets/resource-job-board.png'),
       },
-      first_year_guide: {
+      fy_guide: {
         link: 'https://media.csesoc.org.au/first-year-guide/',
         title: 'First Year Guide',
         description: 'The ultimate guide to conquering first year at CSE.',
@@ -109,7 +111,7 @@ export default {
         description: 'Trimster timetabling tool - no more timetable tangles!',
         src: require('../../assets/resource-notangles.png'),
       },
-      pre_enrollment_guide: {
+      pe_guide: {
         link: 'https://notangles.csesoc.unsw.edu.au/',
         title: 'Pre-enrollment Guide',
         description: 'Learn how to get a headstart on uni',
@@ -143,10 +145,10 @@ export default {
         height: 100%;
         overflow: hidden;
 
-        &.big{
+        &.primary{
           @include linearGradient($primary-color, $secondary-color-2);
         }
-        &.small{
+        &.secondary{
           @include linearGradient($primary-color, $secondary-color-1);
         }
 
