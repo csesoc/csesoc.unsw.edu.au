b28538c13785c25c5813cf20fc704a10a618b230
Mun Joon Teo <teomunjoon101@gmail.com>
2022-01-21 18:37:04 +1100

    Fixed padding and compclub box

--- frontend/src/views/Home/StudentResources.vue
+++ frontend/src/views/Home/StudentResources.vue
@@ -11,69 +11,99 @@
       <HeaderTitle title="STUDENT RESOURCES" />
 
       <v-row justify="space-between">
-        <v-col cols="12" sm="4"> <!-- Jobs -->
+        <v-col cols="12" sm="4">
+          <!-- Jobs -->
           <a target="_blank" :href="resources.jobs_board.link">
-          <div data-cy="resources-box" class="box primary rounded-lg">
-            <h2>{{resources.jobs_board.title}}</h2>
-            <h3>{{resources.jobs_board.description}}</h3>
-            <v-img data-cy="resources-img" max-height="80px" position='top'
-              :src="resources.jobs_board.src" class="image hidden-xs-only" />
-          </div>
+            <div data-cy="resources-box" class="box primary rounded-lg">
+              <h2>{{ resources.jobs_board.title }}</h2>
+              <h3>{{ resources.jobs_board.description }}</h3>
+              <v-img
+                data-cy="resources-img"
+                max-height="80px"
+                position="top"
+                :src="resources.jobs_board.src"
+                class="image hidden-xs-only"
+              />
+            </div>
           </a>
         </v-col>
-        <v-col cols="12" sm="4"> <!-- CompClub -->
+        <v-col cols="12" sm="4">
+          <!-- CompClub -->
           <a target="_blank" :href="resources.comp_club.link">
-          <div data-cy="resources-box" class="box secondary rounded-lg">
-            <h2>{{resources.comp_club.title}}</h2>
-            <h3 class="hidden-sm-above">{{resources.comp_club.description}}</h3>
-            <div class="flex-grow-1"></div>
-            <v-img data-cy="resources-img" max-height="80px" position='top'
-              :src="resources.comp_club.src"  class="image hidden-xs-only" />
-          </div>
+            <div data-cy="resources-box" class="box secondary rounded-lg">
+              <h2>{{ resources.comp_club.title }}</h2>
+              <h3 class="hidden-sm-above">{{ resources.comp_club.description }}</h3>
+              <div class="flex-grow-1"></div>
+              <v-img
+                data-cy="resources-img"
+                max-height="80px"
+                position="top"
+                :src="resources.comp_club.src"
+                class="image hidden-xs-only"
+              />
+            </div>
           </a>
         </v-col>
-        <v-col cols="12" sm="4"> <!-- Notangles -->
+        <v-col cols="12" sm="4">
+          <!-- Notangles -->
           <a target="_blank" :href="resources.notangles.link">
-          <div data-cy="resources-box" class="box primary rounded-lg">
-            <h2>{{resources.notangles.title}}</h2>
-            <h3>{{resources.notangles.description}}</h3>
-            <v-img data-cy="resources-img" max-height="80px"
-              :src="resources.notangles.src" position='top' class="image hidden-xs-only" />
-          </div>
+            <div data-cy="resources-box" class="box primary rounded-lg">
+              <h2>{{ resources.notangles.title }}</h2>
+              <h3>{{ resources.notangles.description }}</h3>
+              <v-img
+                data-cy="resources-img"
+                max-height="80px"
+                :src="resources.notangles.src"
+                position="top"
+                class="image hidden-xs-only"
+              />
+            </div>
           </a>
         </v-col>
-        <v-col cols="12" sm="12"> <!-- Media -->
+        <v-col cols="12" sm="12">
+          <!-- Media -->
           <a target="_blank" :href="resources.media.link">
-          <div data-cy="resources-box" class="box secondary rounded-lg">
-            <h2>{{resources.media.title}}</h2>
-            <h3>{{resources.media.description}}</h3>
-            <v-img data-cy="resources-img" max-height="100px"
-              :src="resources.media.src" position='top' class="image hidden-xs-only" />
-          </div>
+            <div data-cy="resources-box" class="box secondary rounded-lg">
+              <h2>{{ resources.media.title }}</h2>
+              <h3>{{ resources.media.description }}</h3>
+              <v-img
+                data-cy="resources-img"
+                max-height="100px"
+                :src="resources.media.src"
+                position="top"
+                class="image hidden-xs-only"
+              />
+            </div>
           </a>
         </v-col>
-        <v-col cols="12" sm="6"> <!-- First year guide -->
+        <v-col cols="12" sm="6">
+          <!-- First year guide -->
           <a target="_blank" :href="resources.fy_guide.link">
-          <div data-cy="resources-box" class="box primary rounded-lg">
-            <h2>{{resources.fy_guide.title}}</h2>
-            <h3>{{resources.fy_guide.description}}</h3>
-            <v-img data-cy="resources-img"
-              :src="resources.fy_guide.src" position='top' class="image hidden-xs-only  flex-grow-1"/>
-          </div>
+            <div data-cy="resources-box" class="box primary rounded-lg">
+              <h2>{{ resources.fy_guide.title }}</h2>
+              <h3>{{ resources.fy_guide.description }}</h3>
+              <v-img
+                data-cy="resources-img"
+                :src="resources.fy_guide.src"
+                position="top"
+                class="image hidden-xs-only  flex-grow-1"
+              />
+            </div>
           </a>
         </v-col>
-        <v-col cols="12" sm="6"> <!-- Preenrollment guide -->
+        <v-col cols="12" sm="6">
+          <!-- Preenrollment guide -->
           <a target="_blank" :href="resources.pe_guide.link">
-          <div data-cy="resources-box" class="box secondary rounded-lg">
-            <h2>{{resources.pe_guide.title}}</h2>
-            <h3>{{resources.pe_guide.description}}</h3>
-            <v-img
-              data-cy="resources-img"
-              position="top"
-              :src="resources.pe_guide.src"
-              class="image align hidden-xs-only"
-            />
-          </div>
+            <div data-cy="resources-box" class="box secondary rounded-lg">
+              <h2>{{ resources.pe_guide.title }}</h2>
+              <h3>{{ resources.pe_guide.description }}</h3>
+              <v-img
+                data-cy="resources-img"
+                position="top"
+                :src="resources.pe_guide.src"
+                class="image align hidden-xs-only"
+              />
+            </div>
           </a>
         </v-col>
       </v-row>
@@ -106,25 +136,24 @@ export default {
         link: 'https://media.csesoc.org.au/',
         title: 'CSESOC Media',
         description: 'All things content',
-        src: require('../../assets/resource-media.png'),
+        src: require('../../assets/resource-media.png')
       },
       comp_club: {
-        link: 'compClub',
+        link: 'https://compclub.csesoc.unsw.edu.au/',
         title: 'Comp Club',
-        description: 'Get in contact with one of our partnering societies',
-        // TODO: CompClub photo
+        description: 'Promoting computing to high school students'
       },
       notangles: {
         link: 'https://notangles.csesoc.unsw.edu.au/',
         title: 'Notangles',
         description: 'Trimster timetabling tool - no more timetable tangles!',
-        src: require('../../assets/resource-notangles.png'),
+        src: require('../../assets/resource-notangles.png')
       },
       pe_guide: {
         link: ' https://media.csesoc.org.au/pre-enrolment-guide/',
-        title: 'Pre-enrollment Guide',
+        title: 'Pre-enrolment Guide',
         description: 'Learn how to get a headstart on uni',
-        src: require('../../assets/resource-pre-enrolment-guide.jpg'),
+        src: require('../../assets/resource-pre-enrolment-guide.jpg')
       }
     }
   })
@@ -157,17 +186,18 @@ export default {
         height: 100%;
         overflow: hidden;
         max-height: 250px;
+        padding-top: 2vh;
 
-        &.primary{
+        &.primary {
           @include linearGradient($primary-color, $secondary-color-2);
         }
-        &.secondary{
+        &.secondary {
           @include linearGradient($primary-color, $secondary-color-1);
         }
 
         h2 {
           padding-top: calc($space-xs/2);
-          font-size: 150%;
+          font-size: 1.6rem;
           margin: 0;
           padding-left: 35px;
           padding-right: 35px;
@@ -177,7 +207,7 @@ export default {
         h3 {
           padding-left: 35px;
           padding-right: 35px;
-          font-size: 100%;
+          font-size: 1.2rem;
           // margin-bottom: 0px;
         }
 
@@ -185,7 +215,7 @@ export default {
           border-bottom-left-radius: 10px;
           border-bottom-right-radius: 10px;
           top: 0;
-          transition: transform .2s;
+          transition: transform 0.2s;
         }
       }
 
