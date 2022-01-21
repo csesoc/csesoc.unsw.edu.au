9729f62feca965ba4bbe61716e488df19efa2876
Mun Joon Teo <teomunjoon101@gmail.com>
2022-01-21 19:24:56 +1100

    Replaced infinite slideshow pics

--- frontend/src/views/Home/Slideshow.vue
+++ frontend/src/views/Home/Slideshow.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="content hidden-sm-and-down">
     <InfiniteSlideshow duration="32s" direction="reverse">
-    <img
-      height="250px"
-      v-for="banner in banners"
-      :key="banner.src"
-      :src="banner.src"
-      style="padding-left: 20px; padding-right: 20px;"
-    >
+      <img
+        height="250px"
+        v-for="banner in banners"
+        :key="banner.src"
+        :src="banner.src"
+        style="padding-left: 20px; padding-right: 20px;"
+      />
     </InfiniteSlideshow>
   </div>
 </template>
@@ -17,20 +17,26 @@ import InfiniteSlideshow from '@/components/InfiniteSlideshow';
 
 export default {
   components: {
-    InfiniteSlideshow,
+    InfiniteSlideshow
   },
   data: () => ({
     banners: [
-      { src: require('../../assets/banner-1.jpg') },
-      { src: require('../../assets/banner-2.jpg') },
-      { src: require('../../assets/banner-3.jpg') },
-      { src: require('../../assets/banner-4.jpg') }],
-  }),
+      { src: require('../../assets/b1.png') },
+      { src: require('../../assets/b2.png') },
+      { src: require('../../assets/b3.png') },
+      { src: require('../../assets/b4.png') },
+      { src: require('../../assets/b5.png') },
+      { src: require('../../assets/b6.png') },
+      { src: require('../../assets/b7.png') },
+      { src: require('../../assets/b8.png') },
+      { src: require('../../assets/b9.jpg') }
+    ]
+  })
 };
 </script>
 
 <style lang="scss" scoped>
 .content {
-    flex-direction: column;
+  flex-direction: column;
 }
 </style>
