12d2366d0826794e5b47d8ec43d8da481000e99c
Jake Renzella <j@kerenzella.com>
2022-03-19 01:32:15 +1100

    new: (WIP) add  imagemin-webp-webpack-plugin

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,22 @@
+const ImageminWebpWebpackPlugin = require('imagemin-webp-webpack-plugin');
+
 module.exports = {
+  configureWebpack: {
+    plugins: [
+      // Make sure that the plugin is after any plugins that add images
+      new ImageminWebpWebpackPlugin(['src/assets/*'], {
+        overrideExtension: true,
+        config: [
+          {
+            test: /\.(jpe?g|png|gif)/,
+            options: {
+              quality: 75
+            }
+          }
+        ]
+      })
+    ]
+  },
   css: {
     loaderOptions: {
       scss: {
