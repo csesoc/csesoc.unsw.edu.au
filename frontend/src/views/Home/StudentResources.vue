571b2c880482a966d84ad448187221ba19728f9d
hdphuong <vuonghdphuong@gmail.com>
2022-11-25 06:40:28 +1100

    fix overflow scroll

--- frontend/src/views/Home/StudentResources.vue
+++ frontend/src/views/Home/StudentResources.vue
@@ -181,7 +181,7 @@ export default {
         display: flex;
         flex-direction: column;
         height: 100%;
-        overflow: scroll;
+        overflow: hidden;
         max-height: 250px;
         padding-top: 2vh;
 
