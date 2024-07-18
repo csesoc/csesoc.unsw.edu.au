2cb973a4e6f3f94e070c590dba3e76c69cc77d6e
Chris Yoo <chris.yoo@safetyculture.io>
2024-07-18 18:11:39 +1000

    Add Google Analytics

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,16 @@
 import Vue from 'vue';
+import VueGtag from 'vue-gtag';
 import App from './App';
 import router from './plugins/router';
 import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
 // Vue.prototype.$log = console.log;
+
+Vue.use(VueGtag, {
+  // The G-tag is not a secret and is available to view by the public
+  config: { id: 'G-3D5099EXGN' },
+});
 
 new Vue({
   router,
